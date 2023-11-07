1f5faadcf8803304e2599a375d6c3acb49ad0c94
Bilic <flaviu.bilic@student.vinci.be>
2023-11-07 13:10:57 +0100

    fixed footer into HomePage.js

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,14 @@
 const HomePage = () => {
   const main = document.querySelector('main');
   main.innerHTML = 'random stuff go!';
+
+  const footer = document.querySelector('footer');
+  footer.innerHTML = `<div class="row">
+  <div class="col footerColor py-3 text-center">
+    Créé par Hubert Lopaciuk, Igor Sterniczuk, Robert Ailenei, Flaviu Bilic et Laurent
+    Beckman.
+  </div>
+</div>`
 };
 
 export default HomePage;
