1c7d476a075501c94a0dc64bf3afa7010c7d80c1
Beckman <laurent.beckman@student.vinci.be>
2023-11-10 09:37:31 +0100

    StartScene : modif + add assets

--- frontend/src/Components/Game/StartScene.js
+++ frontend/src/Components/Game/StartScene.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import logoAsset from '../../assets/ARMADA_ASSAULT_LOGO_TEXT_NOBACKGROUND.png';
-import startbackgroundImage from '../../assets/start_background.png'; 
+
 
 export default class StartUI extends Phaser.Scene {
     constructor() {
@@ -9,16 +9,13 @@ export default class StartUI extends Phaser.Scene {
 
     preload(){
         this.load.image('logoAsset', logoAsset);
-        this.load.image('startbackgroundImage', startbackgroundImage); // Chargez l'image de fond
+       
     }
 
     create() {
-        // Ajouter l'image de fond
-        const background = this.add.image(this.scale.width, this.scale.height, 'startbackgroundImage').setOrigin(0);
-        background.setDisplaySize(this.scale.width, this.scale.height);
 
         // Charger le logo
-        const logo = this.add.image(this.scale.width * 0.5, this.scale.height * 0.5, 'logoAsset').setOrigin(0.5).setDepth(-1);
+        const logo = this.add.image(this.scale.width * 0.5, this.scale.height * 0.45, 'logoAsset').setOrigin(0.5).setDepth(-1);
 
         // Ajuster la luminosité du logo (augmenter la clarté)
         logo.setTint(0xffffff); // La valeur 0xffffff représente le blanc pur.
@@ -26,16 +23,18 @@ export default class StartUI extends Phaser.Scene {
         // Créer le texte "JOUER" sous le logo
         const jouerText = this.add.text(this.scale.width * 0.5, logo.y + logo.displayHeight, 'JOUER',
             {
-                fontFamily: 'Candara, Arial',
-                fontSize: '48px',
+                fontFamily: 'Blackletter, serif',
+                fontSize: '40px',
                 color: '#ffffff',
                 fontStyle: 'bold'
             }
         ).setOrigin(0.5);
 
+
+    
         jouerText.setInteractive();
-        jouerText.on('pointerover', () => { jouerText.setFontSize(60); });
-        jouerText.on('pointerout', () => { jouerText.setFontSize(48); });
+        jouerText.on('pointerover', () => { jouerText.setFontSize(52); jouerText.setColor('#ffff99');});
+        jouerText.on('pointerout', () => { jouerText.setFontSize(40);  jouerText.setColor('#ffffff');});
         jouerText.on('pointerdown', () => {
             this.scene.start('game-scene');
         });
