a00d00dc08d3d37ae6739b740c2972ff7f8b8ce5
hubert-lopaciuk-vinci <hubert.lopaciuk@student.vinci.be>
2023-11-11 18:20:40 +0100

    add: LoginPage frontend

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,13 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import Classement from '../Pages/Classement';
+import LoginPage from '../Pages/LoginPage'
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
   '/classement': Classement,
+  '/login': LoginPage,
 };
 
 export default routes;
