35d217e1e0ab08518580c3b80c125285fc2da96d
Flaviu Bilic <flaviu.bilic@student.vinci.be>
2023-11-11 17:10:01 +0100

    updated wiki

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -3,36 +3,51 @@ import StartScene from '../Game/StartScene';
 import GameScene from '../Game/GameScene';
 import PauseScene from '../Game/PauseScene';
 
+import archerLogo from '../../assets/archerCard.png';
+import botLogo from '../../assets/botCard.png';
+import knightLogo from '../../assets/knightCard.png';
+import necromancerLogo from '../../assets/necromancerCard.png';
+import warriorLogo from '../../assets/warriorCard.png';
+
+import archerWiki from '../../assets/artwork_wiki/archerWiki.png';
 
 let game;
 
 const GamePage = () => {
   const wikiContent = `<div class="d-flex flex-wrap card-container">
-  <div class="card text-white bg-primary m-3" style="max-width: 18rem;">
-    <div class="card-header">Header</div>
+  <div class="card text-white bg-success m-3 border-danger" style="max-width: 18rem; border-width: 3px;">
+    <div class="card-header"><h5>The Archer</h5> <img src="${archerLogo}" height= "70px" class="rounded" alt="archer logo"></div>
     <div class="card-body">
-      <h5 class="card-title">Primary card title</h5>
+      <h5 class="card-title">Stats:</h5>
+      <p class="card-text">~Write stats here~</p>
+    </div>
+    <img class="card-img-bottom" src="${archerWiki}" alt="Card image cap">
+  </div>
+  <div class="card text-white bg-success m-3 border-light" style="max-width: 18rem; border-width: 3px;">
+    <div class="card-header"><h5>The Bot</h5> <img src="${botLogo}" height= "70px" class="rounded" alt="bot logo"></div>
+    <div class="card-body">
+      <h5 class="card-title">Stats:</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
   </div>
-  <div class="card text-white bg-secondary m-3" style="max-width: 18rem;">
-    <div class="card-header">Header</div>
+  <div class="card text-white bg-success m-3 border-warning" style="max-width: 18rem; border-width: 3px;">
+    <div class="card-header"><h5>The Knight</h5> <img src="${knightLogo}" height= "70px" class="rounded" alt="knight logo"></div>
     <div class="card-body">
-      <h5 class="card-title">Secondary card title</h5>
+      <h5 class="card-title">Stats:</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
   </div>
-  <div class="card text-white bg-success m-3" style="max-width: 18rem;">
-    <div class="card-header">Header</div>
+  <div class="card text-white bg-success m-3 border-dark" style="max-width: 18rem; border-width: 3px;">
+    <div class="card-header"><h5>The Necromancer</h5> <img src="${necromancerLogo}" height= "70px" class="rounded" alt="necromancer logo"></div>
     <div class="card-body">
-      <h5 class="card-title">Success card title</h5>
+      <h5 class="card-title">Stats:</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
   </div>
-  <div class="card text-white bg-danger m-3" style="max-width: 18rem;">
-    <div class="card-header">Header</div>
+  <div class="card text-white bg-success m-3 border-primary" style="max-width: 18rem; border-width: 3px;">
+    <div class="card-header"><h5>The Warrior</h5> <img src="${warriorLogo}" height= "70px" class="rounded" alt="warrior logo"></div>
     <div class="card-body">
-      <h5 class="card-title">Danger card title</h5>
+      <h5 class="card-title">Stats:</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
   </div>
