68977eb2d102c9c0d0d54874c96c04285b5c6f34
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2023-11-11 19:02:33 +0100

    fix: name of classement page to RankingPage + router fix

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -44,7 +44,7 @@ function renderNavbar() {
                     <a class="nav-link" href="#" data-uri="/game">Game</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#" data-uri="/classement">Classement</a>
+                  <a class="nav-link" href="#" data-uri="/ranking">Classement</a>
                 </li>                                 
             </ul>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
