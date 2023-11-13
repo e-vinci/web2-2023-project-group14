09114b51733e3f7cbcd5d725c70ba4d83bd1fe4c
Bilic <flaviu.bilic@student.vinci.be>
2023-11-13 09:01:21 +0100

    Fixed: layout + removed game template files

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -1,83 +1,46 @@
 import Phaser from 'phaser';
-import ScoreLabel from './ScoreLabel';
-import BombSpawner from './BombSpawner';
-import skyAsset from '../../assets/sky.png';
-import platformAsset from '../../assets/platform.png';
-import starAsset from '../../assets/star.png';
-import bombAsset from '../../assets/bomb.png';
-import dudeAsset from '../../assets/dude.png';
 import music from '../../assets/music.mp3';
 import soundOnAsset from '../../assets/soundOn.png';
 import soundOffAsset from '../../assets/soundOff.png';
 import backgroundGameAsset from '../../assets/background.png';
-import hud from '../../assets/armadaHUD.png';
-
-const GROUND_KEY = 'ground';
-const DUDE_KEY = 'dude';
-const STAR_KEY = 'star';
-const BOMB_KEY = 'bomb';
+import hudAsset from '../../assets/armadaHUD.png';
 
 class GameScene extends Phaser.Scene {
   constructor() {
     super('game-scene');
-    this.player = undefined;
-    this.cursors = undefined;
-    this.scoreLabel = undefined;
-    this.stars = undefined;
-    this.bombSpawner = undefined;
-    this.gameOver = false;
-    this.soundOn = true;
-    this.soundButton = undefined;
   }
 
   preload() {
-    this.load.image('sky', skyAsset);
     this.load.image('backgroundGame', backgroundGameAsset);
-    this.load.image('hud', hud);
-    this.load.image(GROUND_KEY, platformAsset);
-    this.load.image(STAR_KEY, starAsset);
-    this.load.image(BOMB_KEY, bombAsset);
+    this.load.image('hud', hudAsset);
     this.load.image('soundOn',soundOnAsset);
     this.load.image('soundOff',soundOffAsset);
     this.load.audio('theme', music);
-
-    this.load.spritesheet(DUDE_KEY, dudeAsset, {
-      frameWidth: 32,
-      frameHeight: 48,
-    });
   }
 
   create() {
+    // Background map game
     const backgroundGame = this.add.image(this.scale.width * 0.5, this.scale.height * 0.5, 'backgroundGame').setOrigin(0.5).setDepth(-1);
     backgroundGame.setScale(this.scale.width / backgroundGame.width, this.scale.height / backgroundGame.height);
-    const platforms = this.createPlatforms();
-    this.player = this.createPlayer();
-    this.stars = this.createStars();
-    this.scoreLabel = this.createScoreLabel(16, 16, 0);
-    this.bombSpawner = new BombSpawner(this, BOMB_KEY);
-    const bombsGroup = this.bombSpawner.group;
-    this.physics.add.collider(this.stars, platforms);
-    this.physics.add.collider(this.player, platforms);
-    this.physics.add.collider(bombsGroup, platforms);
-    this.physics.add.collider(this.player, bombsGroup, this.hitBomb, null, this);
-    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
-    this.cursors = this.input.keyboard.createCursorKeys();
 
-    /* The Collider takes two objects and tests for collision and performs separation against them.
-    Note that we could call a callback in case of collision... */
+    // Hud creation
+    const hudGame = this.add.image(this.scale.width * 0.5, this.scale.height * 0.5, 'hud').setOrigin(0.5).setDepth(1);
+    hudGame.setScale(this.scale.width / hudGame.width, this.scale.height / hudGame.height);
 
      // Add sound toggle button
      const musicT = this.sound.add('theme');
      musicT.play();
-     this.soundButton = this.add.image(this.sys.game.config.width - 30, 30, this.soundOn ? 'soundOn' : 'soundOff');
+     this.soundButton = this.add.image(this.sys.game.config.width - 30, 450, this.soundOn ? 'soundOn' : 'soundOff');
      this.soundButton.setInteractive();
      this.soundButton.on('pointerdown', this.toggleSound, this);
+     this.soundButton.on('pointerover', () => {this.soundButton.setScale(1.2)});
+     this.soundButton.on('pointerout', () => {this.soundButton.setScale(1)});
 
-    const pauseButton = this.add.text(10, 500, 'Pause', { font: '24px Arial', fill: '#ffffff' });
+    const pauseButton = this.add.text(10, 450, 'Pause', { font: '24px Arial', fill: '#ffffff' });
     pauseButton.setInteractive();
     
-    pauseButton.on('pointerover', () => { pauseButton.setFontSize(30); pauseButton.setColor('#ffff99');pauseButton.setX(10);pauseButton.setY(495)});
-    pauseButton.on('pointerout',  () => { pauseButton.setFontSize(24); pauseButton.setColor('#ffffff');pauseButton.setX(10);pauseButton.setY(500)});
+    pauseButton.on('pointerover', () => { pauseButton.setFontSize(30); pauseButton.setColor('#ffff99');pauseButton.setX(10);pauseButton.setY(445)});
+    pauseButton.on('pointerout',  () => { pauseButton.setFontSize(24); pauseButton.setColor('#ffffff');pauseButton.setX(10);pauseButton.setY(450)});
     pauseButton.on('pointerdown', () => {
       this.scene.launch('PauseScene');
       this.scene.pause();
@@ -98,114 +61,6 @@ class GameScene extends Phaser.Scene {
     }
   }
 
-  update() {
-    if (this.gameOver) {
-      return;
-    }
-
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play('left', true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play('right', true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play('turn');
-    }
-
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
-  }
-
-  createPlatforms() {
-    const platforms = this.physics.add.staticGroup();
-
-    platforms.create(400, 568, GROUND_KEY).setScale(2).refreshBody();
-
-    platforms.create(600, 400, GROUND_KEY);
-    platforms.create(50, 250, GROUND_KEY);
-    platforms.create(750, 220, GROUND_KEY);
-    return platforms;
-  }
-
-  createPlayer() {
-    const player = this.physics.add.sprite(100, 450, DUDE_KEY);
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    /* The 'left' animation uses frames 0, 1, 2 and 3 and runs at 10 frames per second.
-    The 'repeat -1' value tells the animation to loop.
-    */
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: 'turn',
-      frames: [{ key: DUDE_KEY, frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers(DUDE_KEY, { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    return player;
-  }
-
-  createStars() {
-    const stars = this.physics.add.group({
-      key: STAR_KEY,
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 },
-    });
-
-    stars.children.iterate((child) => {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-
-    return stars;
-  }
-
-  collectStar(player, star) {
-    star.disableBody(true, true);
-    this.scoreLabel.add(10);
-    if (this.stars.countActive(true) === 0) {
-      //  A new batch of stars to collect
-      this.stars.children.iterate((child) => {
-        child.enableBody(true, child.x, 0, true, true);
-      });
-    }
-
-    this.bombSpawner.spawn(player.x);
-  }
-
-  createScoreLabel(x, y, score) {
-    const style = { fontSize: '32px', fill: '#000' };
-    const label = new ScoreLabel(this, x, y, score, style);
-    console.log('score:', label);
-    this.add.existing(label);
-
-    return label;
-  }
-
-  hitBomb(player) {
-    this.scoreLabel.setText(`GAME OVER : ( \nYour Score = ${this.scoreLabel.score}`);
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    this.gameOver = true;
-  }
 }
 
 export default GameScene;
