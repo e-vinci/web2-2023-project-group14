afa750b6e7ba215e3f5e4543953710205af99281
Bilic <flaviu.bilic@student.vinci.be>
2023-11-14 14:34:15 +0100

    Fixed: Footer error

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,9 +1,15 @@
-const footer = document.querySelector('footer');
-footer.innerHTML = `<div class="row">
-  <div class="col footerColor py-3 text-center">
-    Créé par Hubert Lopaciuk, Igor Sterniczuk, Robert Ailenei, Flaviu Bilic et Laurent
-    Beckman.
-  </div>
-</div>`;
+const Footer = () => {
+  renderFooter();
+};
 
-export default footer;
+function renderFooter(){
+  const footer = document.querySelector('footer');
+  footer.innerHTML = `<div class="row">
+    <div class="col footerColor py-3 text-center">
+      Créé par Hubert Lopaciuk, Igor Sterniczuk, Robert Ailenei, Flaviu Bilic et Laurent
+      Beckman.
+    </div>
+  </div>`;
+};
+
+export default Footer;
