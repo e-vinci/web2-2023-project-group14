0bde8e6e05bbb4de88ddeff1b493e827da1b61ca
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2023-11-16 16:28:53 +0100

    add: table for ranking but without style

--- frontend/src/Components/Pages/RankingPage.js
+++ frontend/src/Components/Pages/RankingPage.js
@@ -9,22 +9,54 @@ const RankingPage = () => {
 
 function renderGoBackHomeButton() {
   const main = document.querySelector('main');
-  const submit = document.createElement('input');
-  submit.value = 'Go back to HomePage';
-  submit.className = 'btn btn-secondary mt-3';
-  submit.addEventListener('click', () => {
+  const backButton = document.createElement('input');
+  backButton.value = 'Go back to HomePage';
+  backButton.className = 'btn btn-secondary mt-3';
+  backButton.addEventListener('click', () => {
     Navigate('/');
   });
 
-  main.appendChild(submit);
+  main.appendChild(backButton);
 }
 
 function renderRankingTable() {
   const main = document.querySelector('main');
-  const submit = document.createElement('input');
-  submit.value = 'Go back to HomePage';
-  submit.className = 'btn btn-secondary mt-3';
-  main.appendChild(submit);
+
+  // Assuming you want to create a table, you can use the following example code
+  const table = document.createElement('table');
+  table.className = 'ranking-table';
+
+  // Create table headers
+  const headers = ['Rank', 'Player', 'Wins'];
+  const headerRow = document.createElement('tr');
+
+  headers.forEach(headerText => {
+    const th = document.createElement('th');
+    th.textContent = headerText;
+    headerRow.appendChild(th);
+  });
+
+  table.appendChild(headerRow);
+
+  // Create sample data (replace this with your actual ranking data)
+  const rankingData = [
+    { rank: 1, player: 'Player 1', wins: 10 },
+    { rank: 2, player: 'Player 2', wins: 8 },
+    // Add more rows as needed
+  ];
+
+  // Create table rows with ranking data
+  rankingData.forEach(data => {
+    const row = document.createElement('tr');
+    Object.values(data).forEach(value => {
+      const td = document.createElement('td');
+      td.textContent = value;
+      row.appendChild(td);
+    });
+    table.appendChild(row);
+  });
+
+  main.appendChild(table);
 }
 
 export default RankingPage;
