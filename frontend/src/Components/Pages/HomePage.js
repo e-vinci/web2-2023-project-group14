c9d41b49cb2a88a1c30150c28c50acf7d8b8dc4d
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2023-11-16 16:48:34 +0100

    add: button play now in home

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,31 @@
+import Navigate from '../Router/Navigate';
+import { clearPage } from '../../utils/render';
+import playNowImage from '../../assets/PlayNow.png';
+
 const HomePage = () => {
+  clearPage();
+
+  // Get the main element
   const main = document.querySelector('main');
-  main.innerHTML = 'random stuff go!';
+
+
+  // Create an image element for the "Play Now" button
+  const playNowButton = document.createElement('img');
+  // Use the imported image path
+  playNowButton.src = playNowImage;
+  playNowButton.alt = 'Play Now';
+  playNowButton.className = 'play-now-button';
+
+  // Add a cursor style to the button
+  playNowButton.style.cursor = 'pointer';
+
+  playNowButton.addEventListener('click', () => {
+    // Redirect to the game page when the image is clicked
+    Navigate('/game');
+  });
+
+  // Append the image button to the main element
+  main.appendChild(playNowButton);
 };
 
 export default HomePage;
