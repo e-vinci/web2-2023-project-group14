6c2919eca86bbd49fc3fa0ca6845d7d2b7d76f60
Robert <robert.ailenei@student.vinci.be>
2023-11-23 02:36:08 +0100

    creation fichier js pour animations + fix des animations de necromancier, archer et warrior

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -10,29 +10,9 @@ import Player from './Player';
 import baseSpriteSheet from '../../assets/playerBase.png';
 import PlayerBase, { createPlayerBase, preloadPlayerBase } from './PlayerBase';
 import MobP1Ex from '../../assets/mobPlayer1Ex.png';
+import {preloadSpriteSheets, createArcherAnim, createNecroAnim, createKnightAnim, createWarriorAnim, createEXTAnim} from './Animations'
 
-// Warrior imports
-import warriorRunSpriteSheet from '../../assets/sprites/NightborneSprites/NightBorneRun.png';
-import warriorHitSpriteSheet from '../../assets/sprites/NightborneSprites/NightBorneHit.png';
-import warriorDeathSpriteSheet from '../../assets/sprites/NightborneSprites/NightBorneDeath.png';
 
-// Exterminator imports
-import extMoveSpriteSheet from '../../assets/sprites/ExterminatorSprites/EXTmove.png';
-import extDeathSpriteSheet from '../../assets/sprites/ExterminatorSprites/EXTdeath.png';
-import extAttackSpriteSheet from '../../assets/sprites/ExterminatorSprites/EXTattack.png';
-
-// Knight Imports
-import knightRunSpriteSheet from '../../assets/sprites/KnightSprites/KnightRun.png';
-import knightAttackSpriteSheet from '../../assets/sprites/KnightSprites/KnightAttack.png';
-import knightDeathSpriteSheet from '../../assets/sprites/KnightSprites/KnightDeath.png';
-
-// Necro Imports
-import necroRunSpriteSheet from '../../assets/sprites/NecroSprites/NecroRun.png';
-import necroDeathSpriteSheet from '../../assets/sprites/NecroSprites/NecroDeath.png';
-import necroAttackSpriteSheet from '../../assets/sprites/NecroSprites/NecroAttack.png';
-
-// Archer Imports
-import redSpriteSheet from '../../assets/sprites/RedSprites/Archer.png';
 
 // All the key 
 const KNIGHT_KEY = 'knight';
@@ -70,69 +50,8 @@ class GameScene extends Phaser.Scene {
     this.load.image('soundOff', soundOffAsset);
     this.load.audio('theme', music);
     
-    // Archer Loads
-
-    this.load.spritesheet('ArcherAll', redSpriteSheet, {
-      frameWidth: 112,
-      frameHeight: 133,
-    });
-
-    // Necro Loads
-    this.load.spritesheet('NecroAttack', necroAttackSpriteSheet, {
-      frameWidth: 160,
-      frameHeight: 103,
-    });
-    this.load.spritesheet('NecroRun', necroRunSpriteSheet, {
-      frameWidth: 160,
-      frameHeight: 103,
-    });
-    this.load.spritesheet('NecroDeath', necroDeathSpriteSheet, {
-      frameWidth: 160,
-      frameHeight: 103,
-    });
-
-    // Knight Loads
-    this.load.spritesheet('KnightAttack', knightAttackSpriteSheet, {
-      frameWidth: 288,
-      frameHeight: 128,
-    });
-    this.load.spritesheet('KnightRun', knightRunSpriteSheet, {
-      frameWidth: 288,
-      frameHeight: 124,
-    });
-    this.load.spritesheet('KnightDeath', knightDeathSpriteSheet, {
-      frameWidth: 288,
-      frameHeight: 80,
-    });
-
-    // Warrior Loads
-    this.load.spritesheet('NightBorneRun', warriorRunSpriteSheet, {
-      frameWidth: 80,
-      frameHeight: 64,
-    });
-    this.load.spritesheet('NightBorneHit', warriorHitSpriteSheet, {
-      frameWidth: 80,
-      frameHeight: 64,
-    });
-    this.load.spritesheet('NightBorneDeath', warriorDeathSpriteSheet, {
-      frameWidth: 80,
-      frameHeight: 64,
-    });
-
-    // Exterminator Loads
-    this.load.spritesheet('ExterminatorMove', extMoveSpriteSheet, {
-      frameWidth: 100,
-      frameHeight: 26,
-    });
-    this.load.spritesheet('ExterminatorDeath', extDeathSpriteSheet, {
-      frameWidth: 100,
-      frameHeight: 26,
-    });
-    this.load.spritesheet('ExterminatorAttack', extAttackSpriteSheet, {
-      frameWidth: 100,
-      frameHeight: 26,
-    });
-
+    // preloading SpriteSheets
+    preloadSpriteSheets(this);
     // preloading cards assets
     preloadCards(this);
 
@@ -146,45 +65,21 @@ class GameScene extends Phaser.Scene {
  cursors = this.input.keyboard.createCursorKeys();
     createPlayerBase(this);
 
-  // Base Animation Creation
-// Archer Creates
-// Archer Run Animation Creation
+  // creating archer animations 
+  createArcherAnim(this);
 
-this.anims.create({
-  key: 'RedRun',
-  frames: this.anims.generateFrameNumbers('ArcherAll', { start:0, end : 24}),
-  frameRate: 15,
-  repeat: -1,
-});
+  // creating necro animations
+  createNecroAnim(this);
 
-const ar = this.add.sprite(600, 400, 'ArcherAll');
-ar.play('RedRun').setDepth(1);
+  // creating knight animations
+  createKnightAnim(this);
+  
+  // creating warrior animations 
+  createWarriorAnim(this);
 
-// Archer Attack Animation Creation
-
-this.anims.create({
-  key: 'RedAttack',
-  frames: this.anims.generateFrameNumbers('ArcherAll', { start:25, end : 34}),
-  frameRate: 15,
-  repeat: -1,
-});
-
-const ar2 = this.add.sprite(650, 400, 'ArcherAll');
-ar2.play('RedAttack').setDepth(1);
-
-
-// Archer Death Animation Creation
-
-this.anims.create({
-  key: 'RedDeath',
-  frames: this.anims.generateFrameNumbers('ArcherAll', { start:120, end : 127}),
-  frameRate: 8,
-  repeat: -1,
-});
-
-const ar3 = this.add.sprite(700, 400, 'ArcherAll');
-ar3.play('RedDeath').setDepth(1);
-
+  // creating ext animations
+  createEXTAnim(this);
+  
 // Creating players (will need to modify with backend)
 this.player1= new Player();
 this.player2= new Player();
@@ -193,154 +88,13 @@ createPlayerBase(this);
 player1CharactersGroup = this.physics.add.group();
 player2CharactersGroup = this.physics.add.group();
 
-// Necro Creates
-// Necro Attack Animation Creation
-this.anims.create({
-  key: 'NecAttack',
-  frames: this.anims.generateFrameNumbers('NecroAttack', { start: 0, end: 12 }),
-  frameRate: 7,
-  repeat: -1,
-});
-// Necro Death Animation Creation
-this.anims.create({
-  key: 'NecDeath',
-  frames: this.anims.generateFrameNumbers('NecroDeath', { start: 0, end: 8 }),
-  frameRate: 7,
-  repeat: -1,
-});
-// Necro Run Animation Creation
-this.anims.create({
-  key: 'NecRun',
-  frames: this.anims.generateFrameNumbers('NecroRun', { start: 0, end: 7 }),
-  frameRate: 7,
-  repeat: -1,
-});
-this.necro = this.physics.add.sprite(450, 300, 'NecroRun');
-this.necro.x=450;
-this.necro.setInteractive(this.input.makePixelPerfect());
-this.necro.play('NecRun').setDepth(1);
-
-const nc2 = this.add.sprite(500, 300, 'NecroDeath');
-nc2.play('NecDeath').setDepth(1);
-
-const nc3 = this.add.sprite(550, 300, 'NecroAttack');
-nc3.play('NecAttack').setDepth(1);
 
 
-// Knight Creates
-// Knight Attack Animation Creation
-this.anims.create({
-  key: 'KnighAttack',
-  frames: this.anims.generateFrameNumbers('KnightAttack', { start: 0, end: 17 }),
-  frameRate: 14,
-  repeat: -1,
-});
-  // Knight Run Animation Creation
-   this.anims.create({
-    key: 'KnighRun',
-    frames: this.anims.generateFrameNumbers('KnightRun', { start: 0, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
 
-  // Knight Run Animation Creation
-  this.anims.create({
-    key: 'KnighDeath',
-    frames: this.anims.generateFrameNumbers('KnightDeath', { start: 0, end: 12 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-  const kn = this.add.sprite(300, 200, 'KnightRun');
-  kn.play('KnighRun').setDepth(1);
 
-  const kn2 = this.add.sprite(360, 200, 'KnightAttack');
-  kn2.play('KnighAttack').setDepth(1);
-  
-  const kn3 = this.add.sprite(450, 200, 'KnightDeath');
-  kn3.play('KnighDeath').setDepth(1);
-
-    // Warrior Creates
-
-    // Warrior Run Animation Creation
-    this.anims.create({
-      key: 'WarriorRun',
-      frames: this.anims.generateFrameNumbers('NightBorneRun', { start: 0, end: 5 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    // Warrior Hit Animation Creation
-    this.anims.create({
-      key: 'WarriorHit',
-      frames: this.anims.generateFrameNumbers('NightBorneHit', { start: 0, end: 12 }),
-      frameRate: 15,
-      repeat: -1,
-    });
-
-    // Warrior Death Animation Creation
-    this.anims.create({
-      key: 'WarriorDeath',
-      frames: this.anims.generateFrameNumbers('NightBorneDeath', { start: 0, end: 24 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    // Creates de Exterminator
-
-    // Exterminator move animation
-
-    this.anims.create({
-      key: 'ExtMove',
-      frames: this.anims.generateFrameNumbers('ExterminatorMove', { start: 0, end: 7 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    // Exterminator Death animation
-
-    this.anims.create({
-      key: 'ExtDeath',
-      frames: this.anims.generateFrameNumbers('ExterminatorDeath', { start: 0, end: 5 }),
-      frameRate: 7,
-      repeat: -1,
-    });
-
-    // Exterminator Attack Animation
-
-    this.anims.create({
-      key: 'ExtAttack',
-      frames: this.anims.generateFrameNumbers('ExterminatorAttack', { start: 0, end: 4 }),
-      frameRate: 7,
-      repeat: -1,
-    });
-
-    // Instances de Exterminator
-    // eslint-disable-next-line prefer-const
-    let ext = this.add.sprite(300, 300, 'ExterminatorMove');
-    ext.play('ExtMove').setDepth(1);
-
-    // eslint-disable-next-line prefer-const
-    let ext2 = this.add.sprite(300, 350, 'ExterminatorDeath');
-    ext2.play('ExtDeath').setDepth(1);
-
-    // eslint-disable-next-line prefer-const
-    let ext3 = this.add.sprite(300, 400, 'ExterminatorAttack');
-    ext3.play('ExtAttack').setDepth(1);
-
-    // Instances de warrior
-
-    // eslint-disable-next-line prefer-const
-    let warrior = this.add.sprite(100, 200, 'NightBorneRun');
-    warrior.play('WarriorRun').setDepth(1);
-
-    // eslint-disable-next-line prefer-const
-    let warrior2 = this.add.sprite(100, 250, 'NightBorneHit');
-    warrior2.play('WarriorHit').setDepth(1);
-
-    // eslint-disable-next-line prefer-const
-    let warrior3 = this.add.sprite(100, 300, 'NightBorneDeath');
-    warrior3.play('WarriorDeath').setDepth(1);
-
+ 
+    
+    
     // eslint-disable-next-line no-console
 
     // Background map game
@@ -520,7 +274,7 @@ this.anims.create({
     //  this.physics.moveToObject(knight, 'X', 10),
     // );
 
-  this.necro.x +=0.5 ;
+  this.necro.x +=0 ;
   const screenWidth = this.sys.game.config.width;
 
         // Vérifiez si le nécromancien a atteint la limite de l'écran
