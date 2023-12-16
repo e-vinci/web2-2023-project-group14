2be7d068f360016e8c32453e9eec16c151a3f206
Beckman <laurent.beckman@student.vinci.be>
2023-12-16 19:42:50 +0100

    Fix statsCards ingame to be dynamic

--- frontend/src/Components/Game/CardCreator.js
+++ frontend/src/Components/Game/CardCreator.js
@@ -12,6 +12,20 @@ import knightCardPopup from '../../assets/knightPopUp.png';
 import necromancerCardPopup from '../../assets/necromancerPopUp.png';
 import warriorCardPopup from '../../assets/warriorPopUp.png';
 
+import Archer from './Units/Archer';
+import Exterminator from './Units/Exterminator';
+import Knight from './Units/Knight';
+import Necro from './Units/Necro';
+import Warrior from './Units/Warrior';
+
+const archerStats = Archer.getStats();
+const exterminatorStats = Exterminator.getStats();
+const knightStats = Knight.getStats();
+const necroStats = Necro.getStats();
+const warriorStats = Warrior.getStats();
+
+
+
 // Load the card images
 export function preloadCards(scene) {
   scene.load.image('archerCard', archerCardAsset);
@@ -31,12 +45,142 @@ export function preloadCards(scene) {
 const yValue = 0.907;
 
 export function createCards(scene) {
+
+  const hoverArcherImageP2 = scene.add
+  .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+  font: "16px 'Times New Roman', Times, serif",
+  fill: "#ffffff",
+  backgroundColor: "#000000",
+  padding: { x: 10, y: 5 },
+  lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverArcherImageP2.setText(`Health: ${archerStats.health} hp
+Range: ${archerStats.range} units
+Damage: ${archerStats.damage} ad
+Movement speed: ${archerStats.speed} ms
+Price: ${archerStats.price} gold`);
+
+const hoverWarriorImageP2 = scene.add
+  .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+  font: "16px 'Times New Roman', Times, serif",
+  fill: "#ffffff",
+  backgroundColor: "#000000",
+  padding: { x: 10, y: 5 },
+  lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverWarriorImageP2.setText(`Health: ${warriorStats.health} hp
+Range: ${warriorStats.range} units
+Damage: ${warriorStats.damage} ad
+Movement speed: ${warriorStats.speed} ms
+Price: ${warriorStats.price} gold`);
+
+const hoverBotImageP2 = scene.add
+    .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+    font: "16px 'Times New Roman', Times, serif",
+    fill: "#ffffff",
+    backgroundColor: "#000000",
+    padding: { x: 10, y: 5 },
+    lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverBotImageP2.setText(`Health: ${exterminatorStats.health} hp
+Range: ${exterminatorStats.range} units
+Damage: ${exterminatorStats.damage} ad
+Movement speed: ${exterminatorStats.speed} ms
+Price: ${exterminatorStats.price} gold`);
+
+const hoverNecromancerImageP2 = scene.add
+  .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+  font: "16px 'Times New Roman', Times, serif",
+  fill: "#ffffff",
+  backgroundColor: "#000000",
+  padding: { x: 10, y: 5 },
+  lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverNecromancerImageP2.setText(`Health: ${necroStats.health} hp
+Range: ${necroStats.range} units
+Damage: ${necroStats.damage} ad
+Movement speed: ${necroStats.speed} ms
+Price: ${necroStats.price} gold`);
+
+const hoverKnightImageP2 = scene.add
+    .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+    font: "16px 'Times New Roman', Times, serif",
+    fill: "#ffffff",
+    backgroundColor: "#000000",
+    padding: { x: 10, y: 5 },
+    lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverKnightImageP2.setText(`Health: ${knightStats.health} hp
+Range: ${knightStats.range} units
+Damage: ${knightStats.damage} ad
+Movement speed: ${knightStats.speed} ms
+Price: ${knightStats.price} gold`);
+
+
+
+  
+
   const hoverArcherImageP1 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'archerCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+    .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+    font: "16px 'Times New Roman', Times, serif",
+    fill: "#ffffff",
+    backgroundColor: "#000000",
+    padding: { x: 10, y: 5 },
+    lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverArcherImageP1.setText(`Health: ${archerStats.health} hp
+Range: ${archerStats.range} units
+Damage: ${archerStats.damage} ad
+Movement speed: ${archerStats.speed} ms
+Price: ${archerStats.price} gold`);
 
   // Adding card for the charachters
   const archerCardP1 = scene.add
@@ -53,11 +197,26 @@ export function createCards(scene) {
   });
 
   const hoverBotImageP1 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'botCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+    .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+    font: "16px 'Times New Roman', Times, serif",
+    fill: "#ffffff",
+    backgroundColor: "#000000",
+    padding: { x: 10, y: 5 },
+    lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverBotImageP1.setText(`Health: ${exterminatorStats.health} hp
+Range: ${exterminatorStats.range} units
+Damage: ${exterminatorStats.damage} ad
+Movement speed: ${exterminatorStats.speed} ms
+Price: ${exterminatorStats.price} gold`);
 
   const botCardP1 = scene.add
     .image(scene.scale.width * 0.13, scene.scale.height * yValue, 'botCard')
@@ -73,11 +232,26 @@ export function createCards(scene) {
   });
 
   const hoverKnightImageP1 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'knightCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+    .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+    font: "16px 'Times New Roman', Times, serif",
+    fill: "#ffffff",
+    backgroundColor: "#000000",
+    padding: { x: 10, y: 5 },
+    lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverKnightImageP1.setText(`Health: ${knightStats.health} hp
+Range: ${knightStats.range} units
+Damage: ${knightStats.damage} ad
+Movement speed: ${knightStats.speed} ms
+Price: ${knightStats.price} gold`);
 
   const knightCardP1 = scene.add
     .image(scene.scale.width * 0.21, scene.scale.height * yValue, 'knightCard')
@@ -93,11 +267,26 @@ export function createCards(scene) {
   });
 
   const hoverNecromancerImageP1 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'necromancerCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+  .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+  font: "16px 'Times New Roman', Times, serif",
+  fill: "#ffffff",
+  backgroundColor: "#000000",
+  padding: { x: 10, y: 5 },
+  lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverNecromancerImageP1.setText(`Health: ${necroStats.health} hp
+Range: ${necroStats.range} units
+Damage: ${necroStats.damage} ad
+Movement speed: ${necroStats.speed} ms
+Price: ${necroStats.price} gold`);
 
   const necromancerCardP1 = scene.add
     .image(scene.scale.width * 0.29, scene.scale.height * yValue, 'necromancerCard')
@@ -113,11 +302,26 @@ export function createCards(scene) {
   });
 
   const hoverWarriorImageP1 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'warriorCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+  .text(scene.scale.width * 0.5, scene.scale.height * 0.7, '', {
+  font: "16px 'Times New Roman', Times, serif",
+  fill: "#ffffff",
+  backgroundColor: "#000000",
+  padding: { x: 10, y: 5 },
+  lineSpacing: 5,
+})
+.setOrigin(0.5, 0.5)
+.setDepth(2)
+.setFixedSize(200, 115)
+.setWordWrapWidth(200) 
+.setAlign('center')
+.setAlpha(0.8)
+.setVisible(false); // Initially set to invisible
+
+hoverWarriorImageP1.setText(`Health: ${warriorStats.health} hp
+Range: ${warriorStats.range} units
+Damage: ${warriorStats.damage} ad
+Movement speed: ${warriorStats.speed} ms
+Price: ${warriorStats.price} gold`);
 
   const warriorCardP1 = scene.add
     .image(scene.scale.width * 0.37, scene.scale.height * yValue, 'warriorCard')
@@ -132,12 +336,6 @@ export function createCards(scene) {
     hoverWarriorImageP1.setVisible(false);
   });
 
-  const hoverWarriorImageP2 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'warriorCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
 
   const warriorCardP2 = scene.add
     .image(scene.scale.width * 0.95, scene.scale.height * yValue, 'warriorCard')
@@ -152,12 +350,7 @@ export function createCards(scene) {
     hoverWarriorImageP2.setVisible(false);
   });
 
-  const hoverNecromancerImageP2 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'necromancerCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+
 
   const necromancerCardP2 = scene.add
     .image(scene.scale.width * 0.87, scene.scale.height * yValue, 'necromancerCard')
@@ -172,12 +365,6 @@ export function createCards(scene) {
     hoverNecromancerImageP2.setVisible(false);
   });
 
-  const hoverKnightImageP2 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'knightCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
 
   const knightCardP2 = scene.add
     .image(scene.scale.width * 0.79, scene.scale.height * yValue, 'knightCard')
@@ -192,12 +379,7 @@ export function createCards(scene) {
     hoverKnightImageP2.setVisible(false);
   });
 
-  const hoverBotImageP2 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'botCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+  
 
   const botCardP2 = scene.add
     .image(scene.scale.width * 0.71, scene.scale.height * yValue, 'botCard')
@@ -212,12 +394,7 @@ export function createCards(scene) {
     hoverBotImageP2.setVisible(false);
   });
 
-  const hoverArcherImageP2 = scene.add
-    .image(scene.scale.width * 0.5, scene.scale.height * 0.7, 'archerCardPopUp')
-    .setOrigin(0.5, 0.5)
-    .setDepth(2)
-    .setScale(0.7)
-    .setVisible(false); // Initially set to invisible
+  
 
   const archerCardP2 = scene.add
     .image(scene.scale.width * 0.63, scene.scale.height * yValue, 'archerCard')
