7309f580008440868a6af218886414a61d0d9710
RobertAilenei <robert.ailenei@student.vinci.be>
2023-12-17 03:13:29 +0100

    fixed xarning

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -122,7 +122,7 @@ function renderNavbar() {
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li><a class="dropdown-item" href="#" data-uri="/logout1">Logout</a></li>
                   <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#" id="delete1" data-uri="/delete2">Delete Account</a></li>
+                  <li><a class="dropdown-item" href="#" id="delete1" >Delete Account</a></li>
                 </ul>
               </li>   
               <li class="nav-item">
@@ -173,7 +173,7 @@ function renderNavbar() {
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li><a class="dropdown-item" href="#" data-uri="/logout2">Logout</a></li>
                   <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#" id="delete2" data-uri="/delete2">Delete Account</a></li>
+                  <li><a class="dropdown-item" href="#" id="delete2" >Delete Account</a></li>
                 </ul>
               </li>   
               <li class="nav-item">
@@ -224,7 +224,7 @@ function renderNavbar() {
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
               <li><a class="dropdown-item" href="#" data-uri="/logout1">Logout</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#" data-uri="/delete1">Delete Account</a></li>
+              <li><a class="dropdown-item" href="#" >Delete Account</a></li>
             </ul>
           </li>   
           <li class="nav-item dropdown">
@@ -234,7 +234,7 @@ function renderNavbar() {
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown2">
               <li><a class="dropdown-item" href="#" data-uri="/logout2">Logout</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#" data-uri="/delete2">Delete Account</a></li>
+              <li><a class="dropdown-item" href="#">Delete Account</a></li>
             </ul>
           </li>                       
         </ul>
@@ -263,6 +263,58 @@ function renderNavbar() {
     Navigate('/');
   });
 
+  const deleteAccountButton1 = document.getElementById('delete1');
+  if (deleteAccountButton1) {
+    deleteAccountButton1.addEventListener('click', () => {
+      // Show confirmation dialog
+      const confirmDeletion = window.confirm('Are you sure you want to delete your account?');
+      if (confirmDeletion) {
+        // Invoke the function to delete the account (adjust the function name accordingly)
+        deleteAccountFunction(username1);
+      }
+    });
+  }
+
+  const deleteAccountButton2 = document.getElementById('delete2');
+  if (deleteAccountButton2) {
+    deleteAccountButton2.addEventListener('click', () => {
+      // Show confirmation dialog
+      const confirmDeletion = window.confirm('Are you sure you want to delete your account?');
+      if (confirmDeletion) {
+        // Invoke the function to delete the account (adjust the function name accordingly)
+        deleteAccountFunction(username2);
+      }
+    });
+  }
+
+  function deleteAccountFunction(username) {
+    // Perform the deletion logic here, such as making an AJAX request to your server
+    // You can use the fetch API or any other method to send a request to your server
+    // Example using fetch:
+    fetch(`/api/deleteAccount/${username}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        // Add any other headers if needed (e.g., authentication headers)
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to delete account: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Handle the success response from the server
+        console.log('Account deleted successfully', data);
+        // Redirect to a new page or perform any other actions
+      })
+      .catch(error => {
+        // Handle errors during the deletion process
+        console.error('Error deleting account:', error.message);
+        // Display an error message to the user or perform other error-handling actions
+      });
+  }
 
 
 
